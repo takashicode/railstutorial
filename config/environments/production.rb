3e4c4332769777baa7d28dc74d91872448f8a77f
takashi <Takashi Iwase>
2018-07-11 21:38:09 +0900

    modify account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app100923239@heroku.com'],
+    :password       => ENV['ipngf0xl9779'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
