4a201e5506c5ec970a2ee687013398ea37a7f30e
takashi <Takashi Iwase>
2018-07-11 21:50:08 +0900

    modify account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'dry-brook-59674.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app100923239@heroku.com'],
-    :password       => ENV['ipngf0xl9779'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
